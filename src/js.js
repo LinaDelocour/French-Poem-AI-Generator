b834c4da5dcb6c035d70f4604f48ac2679de38c4
LinaDelocour <polinadorohykh@gmail.com>
2026-06-18 21:00:44 +0100

    Improved user experience, by making the poem section appear only when the poem has been/is being generated. Added a message that the poem is being generated, during the time that AI takes to generate the poem, to reassure the user by demonstrating functionality.

--- src/js.js
+++ src/js.js
@@ -1,11 +1,16 @@
 function generate(event){
     event.preventDefault();
-    let userinstructions = document.querySelector("#user-instructions");
+    let userinstructions = document.querySelector("#user-instructions").value;
     let apiKey = "ae3a312323bto5f9e8049a27984bd0d3";
     let prompt = `Write a 4 - line poem in French on the topic of ${userinstructions}. Separate each line using <br />. Add a signage "SheCodes AI" at the bottom of a poem in <strong> element. Do not add a title.`;
     let context = "You are a romantic expert.";
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
     axios(apiURL).then(displayPoem);
+
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="text-style blink-smooth">Generating a poem about ${userinstructions}...</div>`;
+
 }
 
 function displayPoem(response) {
